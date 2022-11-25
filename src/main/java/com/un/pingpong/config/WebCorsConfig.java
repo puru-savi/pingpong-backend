4566a3357e5e7861048253d0d254eeb8208670d3
Kumar <pkuma19@AP.xom.com>
2022-11-26 01:54:39 +0700

    remove unused import

--- src/main/java/com/un/pingpong/config/WebCorsConfig.java
+++ src/main/java/com/un/pingpong/config/WebCorsConfig.java
@@ -1,7 +1,6 @@
 package com.un.pingpong.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -11,9 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class  WebCorsConfig  extends WebMvcConfigurerAdapter {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("https://angular-login-rest-api-ty5tr1.stackblitz.io")
-//                .allowedMethods(HttpMethod.POST.name(), HttpMethod.GET.name(),HttpMethod.OPTIONS.name());
+
         registry.addMapping("/**")
                 .allowedOrigins("https://angular-login-rest-api-ty5tr1.stackblitz.io")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD","OPTIONS")
